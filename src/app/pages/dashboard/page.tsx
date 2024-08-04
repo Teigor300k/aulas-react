0f830b609f4764778679898d5ed14ec377b43c92
Teigor300k <igorteixeira354@gmail.com>
2024-08-04 10:54:37 +0100

    Linkando paginas

--- src/app/pages/dashboard/page.tsx
+++ src/app/pages/dashboard/page.tsx
@@ -1,7 +1,18 @@
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+
 export const Dashboard = () => {
-  return (
-    <>
-      <p>Page Dashboard</p>
-    </>
-  );
+    const history = useNavigate()
+
+    const handleClick = () => {
+        history("/entrar")
+
+    }
+    return (
+        <>
+            <p>Page Dashboard</p>
+            <Link to="/entrar">Login</Link>
+            <button onClick={handleClick}>Login</button>
+        </>
+    );
 };
--- src/app/pages/login/page.tsx
+++ src/app/pages/login/page.tsx
@@ -1,7 +1,19 @@
+import { Link } from "react-router-dom";
+
 export const Login = () => {
   return (
     <>
-      <p>Page Login</p>
+    <Link to="/pagina-inicial">Dashboard</Link>
+      <form action="" method="post">
+        <div>
+          <label htmlFor="email">Email</label>
+          <input type="text" name="email" id="email" />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" id="email" />
+        </div>
+      </form>
     </>
   );
 };
