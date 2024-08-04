cf3cbeb876fd273b87bf188f4829cbc2380f8280
Teigor300k <igorteixeira354@gmail.com>
2024-08-04 11:19:24 +0100

    Usando o useState

--- src/app/pages/login/page.tsx
+++ src/app/pages/login/page.tsx
@@ -1,18 +1,29 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 
 export const Login = () => {
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const handleClick = () => {
+    console.log(email);
+
+  }
   return (
     <>
-    <Link to="/pagina-inicial">Dashboard</Link>
-      <form action="" method="post">
+      <Link to="/pagina-inicial">Dashboard</Link>
+      <form>
         <div>
           <label htmlFor="email">Email</label>
-          <input type="text" name="email" id="email" />
+          <br />
+          <input type="text" name="email" id="email" value={email} onChange={e => setEmail(e.target.value)} />
         </div>
+        <br />
         <div>
-          <label htmlFor="password">Password</label>
-          <input type="password" name="password" id="email" />
+          <label htmlFor="password">Password</label> 
+          <br />
+          <input type="password" name="password" id="email" value={password} onChange={e => setPassword(e.target.value)} />
         </div>
+        <button type="button" onClick={handleClick}>Entrar</button>
       </form>
     </>
   );
